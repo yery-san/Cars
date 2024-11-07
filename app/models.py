ecf832882e912ca76183a6870df9afb6508fc55b
Palmiére <palmiere.santana@escolar.ifrn.edu.br>
2024-11-07 09:12:18 -0300

    criando modelo car e adicionando ao admin

--- app/models.py
+++ app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Car(models.Model):
+    model = models.CharField(max_length=50, null=False)
+    brand = models.CharField(max_length=50, null=False)
+    year = models.IntegerField(null=False)
+    filename = models.CharField(max_length=50, null=False)
