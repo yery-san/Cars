ad22809de13ab9e0afe7c52247282a454164c87f
Palmiére <palmiere.santana@escolar.ifrn.edu.br>
2024-11-21 07:50:11 -0300

    criação do usuário

--- app/admin.py
+++ app/admin.py
@@ -5,8 +5,8 @@ from app.models import Car
 # Register your models here.
 
 class CarFilter(admin.ModelAdmin):
-    list_display = ("id", "model", "year")
-    list_display_links = ("id", "model", "year")
+    list_display = ("id", "user", "model","brand", "year")
+    list_display_links = ("id", "model", "brand", "year")
     list_filter = ("brand","year")
     search_fields = ("model","brand")
 
--- app/models.py
+++ app/models.py
@@ -1,3 +1,4 @@
+import django
 from django.db import models
 
 # Create your models here.
@@ -7,6 +8,7 @@ class Car(models.Model):
     brand = models.CharField(max_length=50, null=False)
     year = models.IntegerField(null=False)
     filename = models.CharField(max_length=50, null=False)
+    user = models.ForeignKey(django.contrib.auth.get_user_model(), on_delete=models.CASCADE)
     
     def __str__(self):
         return self.model + 'ano' + str(self.year)
